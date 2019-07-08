d6381b5c12e145785d30d741844571fc84ced68b
Brandon Wilburn <brandon.wilburn@wustl.edu>
2019-07-07 23:38:25 -0500

    no desc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.onresize = function() {
-    document.body.height = window.innerHeight;
+    document.body.style.height = window.innerHeight;
 }
 window.onresize(); // called to initially set the height.
